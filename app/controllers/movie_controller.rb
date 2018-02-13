271a2bf414ecc60c9b7ae7ddacd1bcccdd0dc931
almcintyre <almcintyre.eng@gmail.com>
2018-02-12 23:07:31 -0800

    Update average function to work with postgresql

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -22,7 +22,7 @@ class MovieController < ApplicationController
       if @reviews.blank?
         @avg_review= 0
       else
-        @avg_review = @reviews.average(:rating).round(2)
+        @avg_review = @reviews.average('CAST(rating AS INT)').round(2)
       end
       movie.rating = @avg_review
       movie.save
